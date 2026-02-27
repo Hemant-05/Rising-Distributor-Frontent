eaa0b07223f4d9e551a625ec1458cac7ba3a309d
heman <hemantsahu56088@gmail.com>
2026-02-27 13:37:44 +0530

    address system improvement.

---
+++ lib/models/dto/cancel_request.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cancel_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CancelRequest _$CancelRequestFromJson(Map<String, dynamic> json) =>
+    CancelRequest(reason: json['reason'] as String);
+
+Map<String, dynamic> _$CancelRequestToJson(CancelRequest instance) =>
+    <String, dynamic>{'reason': instance.reason};
